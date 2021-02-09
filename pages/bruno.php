ef0e78be4421f49d52bb279341cc4335ee7591f4
Bruno Assis de Tassis <breve0105@ambev.com.br>
2021-02-09 17:02:47 -0300

    Bruno

---
+++ pages/bruno.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula Git/GitHub</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <h1>PAGINA INICIAL DO BRUNO</h1>
+        <img src="https://media.giphy.com/media/FYqf889lXd9Ru/giphy.gif" alt="Aewww" width="128">
+    </div>
+</body>
+
+</html>
