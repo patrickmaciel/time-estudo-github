9886202062d811831cbc7fd81a1cb5a3f90767fe
Patrick Maciel Campos <breve0100@ambev.com.br>
2021-02-09 14:28:34 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/default.css">
+    <title>Estudo Github</title>
+</head>
+<body>
+    <div id="header" class='wrapper'>
+        <p>Menu</p>
+        <ul>
+            <li>
+                <a href="/pages/patrick.php">Patrick</a>
+            </li>
+            <li>
+                <a href="/pages/joyce.php">Joyce</a>
+            </li>
+            <li>
+                <a href="/pages/bruno.php">Bruno</a>
+            </li>
+            <li>
+                <a href="/pages/douglas.php">Douglas</a>
+            </li>
+            <li>
+                <a href="/pages/geuasley.php">Geuasley</a>
+            </li>
+            <li>
+                <a href="/pages/matheus.php">Matheus</a>
+            </li>
+        </ul>
+    </div>
+
+    <div id="content" class='wrapper'>
+        <p>Conteúdo da página</p>
+    </div>
+
+    <div id="footer" class='wrapper'>
+        <p><em>Rodapé da página</em></p>
+    </div>
+
+    <script src="/assets/js/default.js"></script>
+</body>
+</html>
